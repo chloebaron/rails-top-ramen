1099900761084868e1deccd668d394d5e3363742
Chloe Baron <chloe.f.baron@gmail.com>
2018-05-21 15:43:31 -0400

    Navbar and Footer in app.html

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_183929) do
+ActiveRecord::Schema.define(version: 2018_05_21_185630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favourites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "ramen_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ramen_id"], name: "index_favourites_on_ramen_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
+  end
+
+  create_table "ramen", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_ramen_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +50,7 @@ ActiveRecord::Schema.define(version: 2018_05_21_183929) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favourites", "ramen", column: "ramen_id"
+  add_foreign_key "favourites", "users"
+  add_foreign_key "ramen", "users"
 end
