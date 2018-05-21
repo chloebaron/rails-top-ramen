4f81f9e1f7a7c3462d642072c37325e9adace579
linming ye <leaves.woods@hotmail.com>
2018-05-21 17:05:51 -0400

    fix association

--- app/controllers/ramens_controller.rb
+++ app/controllers/ramens_controller.rb
@@ -1,2 +1,37 @@
 class RamensController < ApplicationController
+  before_action :find_ramen, only: [:show, :destroy]
+  def index
+    @ramens = Ramen.all
+  end
+
+  def show
+  end
+
+  def new
+    @ramen = Ramen.new
+  end
+
+  def create
+    user = current_user
+    @ramen = Ramen.new(ramen_params)
+    @ramen.user = user
+    if @ramen.save!
+      redirect_to ramen_path(@ramen)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_ramen
+    @ramen = Ramen.find(params[:id])
+  end
+
+  def ramen_params
+    params.require(:ramen).permit(:name, :description)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :ramens
+  has_many :favourites
+  has_many :fav_ramens, through: :favourites, source: :ramen
 end
