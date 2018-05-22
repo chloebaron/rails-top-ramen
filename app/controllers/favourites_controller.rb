f0759beadc76c0b45fbb28999fcbe42115121919
linming ye <leaves.woods@hotmail.com>
2018-05-22 13:45:56 -0400

    added create for favourite and a fav link in ramen show page

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,2 +1,13 @@
 class FavouritesController < ApplicationController
+
+  def create
+    # ------------------------------
+    # favourite = Favourite.new
+    # favourite.ramen = Ramen.find(params[:id])
+    # favourite.user = current_user
+    # favourite.save!
+    # ------------------------------ both ways would work
+    Favourite.create!(user_id: current_user.id, ramen_id: params[:ramen_id])
+  end
+
 end
