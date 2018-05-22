a08a934bed093845041af3ad9796adca7ab7ee5a
leo marshall <tgbrtmarshall@gmail.com>
2018-05-22 17:12:44 -0400

    created user controller for dashboard method

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/users/:id/dashboard', to: 'users#dashboard'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
