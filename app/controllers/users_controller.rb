7ef863fb0cb0f249c1c4a0ed15a018d6b8f935df
linming ye <leaves.woods@hotmail.com>
2018-05-24 13:27:50 -0400

    add reviews of others and your own in dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
   def dashboard
     @user = current_user
-    @reviews = @user.reviews
-    authorize @reviews
+    @reviews_myself = @user.reviews
+    @reviews_others = Ramen.where(user_id: @user.id).map do |ramen|
+       Review.where(ramen_id: ramen.id)
+    end
+    @reviews_others.flatten!
+    authorize @reviews_myself
+    # authorize @reviews_others unless @reviews_others.nil?
     authorize @user
     #raise
   end
